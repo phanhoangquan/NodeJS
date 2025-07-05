8d7cef4e36fb47d2c464c05623ad51db7517c712
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-07-05 18:53:18 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({
+      extended:true
+}))
+app.use(express.json())
 
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs',handlebars.engine({extname:'.hbs'}))
@@ -17,14 +23,12 @@ app.set('views', path.join(__dirname,'resources/views'))
 
 console.log(path.join(__dirname,'resources/views')) 
 
+//Home,search,contact
 
-app.get('/', (req, res) => {
-      res.render('home')
-})
 
-app.get('/news', (req, res) => {
-      res.render('news')
-})
+//Routes init
+
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
