40f1ef1806b4669d6fd8be95deb08352d32847ff
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-08-09 10:13:55 +0700

    [CRUD] Create new course , Update Course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
+const methodOverride = require('method-override');
 const path = require('path');
 const app = express();
 const port = 3000;
@@ -18,12 +19,20 @@ app.use(
    }),
 );
 app.use(express.json());
+app.use(methodOverride('_method'));
 
 // HTTP logger
 // app.use(morgan('combined'))
 
 //Template engine
-app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
+app.engine(
+   'hbs',
+   handlebars.engine({
+      extname: '.hbs',
+      helpers: { sum: (a, b) => a + b },
+   }),
+);
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
