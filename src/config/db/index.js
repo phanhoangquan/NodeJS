f4cc6806be2e90cd50207e6e873166c608863aed
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-11-12 17:49:15 +0700

    [CRUD] Delete Course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+// Fix [MONGOOSE] DeprecationWarning
+mongoose.set('strictQuery', false);
+
 async function connect() {
    try {
       await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
